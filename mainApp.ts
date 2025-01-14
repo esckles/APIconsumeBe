52d0dfbc863981fdb9c041be3e5207a0c709daa3
esckles <davidsamzzywilliams1234@gmail.com>
2025-01-14 11:55:22 +0100

    try

--- mainApp.ts
+++ mainApp.ts
@@ -4,11 +4,7 @@ export const mainApp = async (app: Application) => {
   try {
     app.use("/api", AuthRouter);
     app.get("/", (req: Request, res: Response) => {
-      try {
-        res.status(200).json({ message: "Welcome to my API", status: 200 });
-      } catch (error) {
-        res.status(404).json({ messsage: "Error to my API", status: 404 });
-      }
+      res.status(200).json({ message: "Welcome to my API", status: 200 });
     });
   } catch (error) {
     return error;
