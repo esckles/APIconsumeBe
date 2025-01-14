64094335ac205566e2fb03bafba0908e27763d5c
esckles <davidsamzzywilliams1234@gmail.com>
2025-01-14 12:56:53 +0100

    cors

--- index.ts
+++ index.ts
@@ -7,8 +7,14 @@ env.config();
 
 const app: Application = express();
 app.use(express.json());
-app.use(cors());
 mainApp(app);
+app.use(
+  cors({
+    origin: "https://e-samstore.onrender.com",
+    methods: ["GET", "POST", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.listen(parseInt(process.env.PORT as string), () => {
   dbConfig();
